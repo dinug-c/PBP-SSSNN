dd984fc6ecc578a2d04c393f14da416b470d170c
Resma Adi Nugroho <resmaadi@students.undip.ac.id>
2023-11-08 02:05:19 +0700

    fix

--- Pertemuan 6/app/src/main/java/id/newtdev/pbp7/MainActivity.kt
+++ Pertemuan 6/app/src/main/java/id/newtdev/pbp7/MainActivity.kt
@@ -11,73 +11,64 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
     // VALIDITY FUNCTION
-    // CHECK LIST OF EDITTEXT
-    private fun checkValidity(value: List<EditText>): Boolean {
+    /// MULTI CHECK
+    private fun multiCheck(value: Map<String, EditText>): Boolean {
         var isValid  = false
-        for (editText in value){
-            if(editText.text.isEmpty()) isValid = true
-        }
+        value.map { isValid = it.value.text.isEmpty() || (it.value.text.isBlank()) }
         return isValid
     }
-    // SINGLE CHECK
-    private fun checkSingleValidity(value: EditText): Boolean {
-        return value.text.isEmpty();
+    // MULTI ERROR SHOW
+    private fun multiError(value: Map<String, EditText>) {
+        value.map {
+            if(it.value.text.isEmpty()) {
+                it.value.error = "${it.key} harus diisi"
+            }
+        }
     }
-
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // INIT VAR BINDING
-        val lebarTxt : EditText = findViewById(R.id.et_width)
-        val panjangTxt : EditText = findViewById(R.id.et_length)
-        val tinggiTxt : EditText = findViewById(R.id.et_height)
+        val LebarTxt : EditText = findViewById(R.id.et_width)
+        val PanjangTxt : EditText = findViewById(R.id.et_length)
+        val TinggiTxt : EditText = findViewById(R.id.et_height)
         val calculateBtn : Button = findViewById(R.id.btn_calculate)
         val luasBtn : Button = findViewById(R.id.btn_calculate_luas)
         val kelBtn : Button = findViewById(R.id.btn_calculate_keliling)
         val resultView : TextView = findViewById(R.id.tv_result)
-
+        val formData : Map<String, EditText> = mapOf(
+            "Lebar" to LebarTxt,
+            "Panjang" to PanjangTxt,
+            "Tinggi" to TinggiTxt
+        )
         // CHECK FORM VALIDATION
-        fun checkFormValidation(callback: (Map<String, Double>) -> Unit) {
-            if(checkSingleValidity(lebarTxt)){
-                lebarTxt.error = "Lebar wajib diisi"
-            }
-            if(checkSingleValidity(panjangTxt)){
-                panjangTxt.error = "Panjang wajib diisi"
-            }
-            if(checkSingleValidity(tinggiTxt)){
-                tinggiTxt.error = "Tinggi wajib diisi"
-            }
-            if(!checkValidity(listOf(lebarTxt, panjangTxt, tinggiTxt))){
-                val lebarValue = lebarTxt.text.toString().toDouble()
-                val panjangValue = panjangTxt.text.toString().toDouble()
-                val tinggiValue = tinggiTxt.text.toString().toDouble()
-                val mapCallback = mapOf("lebar" to lebarValue, "panjang" to panjangValue, "tinggi" to tinggiValue )
-                callback(mapCallback)
+        fun checkFormValidation(form: Map<String, EditText>, callback: (MutableMap<String, Double>) -> Unit) {
+            multiError(form)
+            if(!multiCheck(form)){
+                var data = mutableMapOf<String, Double>()
+                form.map { data.put(it.key, it.value.text.toString().toDouble()) }
+                callback(data)
                 Toast.makeText(this, "Berhasil menghitung", Toast.LENGTH_SHORT).show()
             }
         }
-
         // CALCULATE FUNCTION
         calculateBtn.setOnClickListener {
-            checkFormValidation {
-                val volumeCount = it.getValue("lebar") * it.getValue("panjang") * it.getValue("tinggi")
+            checkFormValidation(formData) {
+                val volumeCount = it.getValue("Lebar") * it.getValue("Panjang") * it.getValue("Tinggi")
                 resultView.text = "Volume: $volumeCount m3"
             }
         }
-
         luasBtn.setOnClickListener {
-            checkFormValidation {
-                val luasCount = 2 * ((it.getValue("panjang")*it.getValue("lebar")) + (it.getValue("panjang")*it.getValue("tinggi")) + (it.getValue("lebar")*it.getValue("tinggi")))
+            checkFormValidation(formData) {
+                val luasCount = 2 * ((it.getValue("Panjang")*it.getValue("Lebar")) + (it.getValue("Panjang")*it.getValue("Tinggi")) + (it.getValue("Lebar")*it.getValue("Tinggi")))
                 resultView.text = "Luas: $luasCount m2"
             }
         }
-
         kelBtn.setOnClickListener {
-            checkFormValidation {
-                val kelCount = 4 *(it.getValue("lebar") + it.getValue("panjang") * it.getValue("tinggi"))
+            checkFormValidation(formData) {
+                val kelCount = 4 *(it.getValue("Lebar") + it.getValue("Panjang") * it.getValue("Tinggi"))
                 resultView.text = "Keliling: $kelCount m"
             }
         }
