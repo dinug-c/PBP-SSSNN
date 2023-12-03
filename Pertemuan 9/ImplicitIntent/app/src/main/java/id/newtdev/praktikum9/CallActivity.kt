e671a193634e2c4374fcc7099132a797399dc1c9
Resma Adi Nugroho <resmaadi@students.undip.ac.id>
2023-12-03 19:04:22 +0700

    feat: tugas prak 10

---
+++ Pertemuan 9/ImplicitIntent/app/src/main/java/id/newtdev/praktikum9/CallActivity.kt
@@ -0,0 +1,11 @@
+package id.newtdev.praktikum9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_call)
+    }
+}
